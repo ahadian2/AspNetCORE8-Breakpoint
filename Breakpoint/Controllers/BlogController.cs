da72b55dceef965b88b811a14093098c946377e7
Mohammadreza <ahadian2@gmail.com>
2024-06-08 13:49:52 +0330

    Add project files.

---
+++ Breakpoint/Controllers/BlogController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Breakpoint.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+            if(id == 0)
+            {
+                id = 20;
+            }
+            int x = id;
+            int y = x + 10;
+            int z = y + x;
+            return Content(z.ToString());
+        }
+    }
+}
